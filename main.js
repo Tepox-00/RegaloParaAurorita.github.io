36ef76bfbddb08b3f2c073171afbc03936671483
Erick <tepox.juegos@gmail.com>
2026-05-22 05:36:20 -0600

    Update main.js

--- main.js
+++ main.js
@@ -114,7 +114,7 @@ const styles = [
     companionFolder: "Assets/Image/Compañeros/McDonald",
     companionCount: 5,
     musicPath: "Assets/Audio/Mcdonald_Music.mp3",
-    musicVolume: 0.05,
+    musicVolume: 0.02,
   },
 ];
 
